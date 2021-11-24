c04fd30f0ae9b60566818b6620a75896c6950d68
Gabriel Lopes <gabriellopes@pop-os.localdomain>
2021-11-24 17:40:21 -0300

    :sparkles: feat: criando conta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Já existe um cadastro com esse CPF, por favor, cadastre um novo" })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).json({ message: "Cadastro criado com sucesso" })
+});
+
+app.get("/statement/:cpf", (req, res) => {
+  const { cpf } = req.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(400).json({ error: "Cliente não encontrado" })
+  }
+
+  return res.json(customer.statement)
+});
 
 app.listen(3333);
