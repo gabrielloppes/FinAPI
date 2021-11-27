0c76e1592c86bf2e2597dec2a5f8580c717f3045
Gabriel Lopes <gabriellopes@pop-os.localdomain>
2021-11-27 01:02:14 -0300

    :sparkles: feat: statement by date

--- src/index.js
+++ src/index.js
@@ -6,13 +6,42 @@ app.use(express.json());
 
 const customers = [];
 
-app.post("/account", (req, res) => {
-  const { cpf, name } = req.body;
+// Middleware
+verifyIfAccountExists = (request, response, next) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Cliente não encontrado" })
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+getBalance = (statement) => {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
+// HTTP Methods
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
 
   if (customerAlreadyExists) {
-    return res.status(400).json({ error: "Já existe um cadastro com esse CPF, por favor, cadastre um novo" })
+    return response.status(400).json({ error: "Já existe um cadastro com esse CPF, por favor, cadastre um novo" })
   }
 
   customers.push({
@@ -22,19 +51,61 @@ app.post("/account", (req, res) => {
     statement: [],
   });
 
-  return res.status(201).json({ message: "Cadastro criado com sucesso" })
+  return response.status(201).json({ message: "Cadastro criado com sucesso" })
 });
 
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params;
+app.get("/statement", verifyIfAccountExists, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement)
+});
 
-  const customer = customers.find(customer => customer.cpf === cpf);
+app.post("/deposit", verifyIfAccountExists, (request, response) => {
+  const { description, amount } = request.body;
 
-  if (!customer) {
-    return res.status(400).json({ error: "Cliente não encontrado" })
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    createdAt: new Date(),
+    type: "credit"
   }
 
-  return res.json(customer.statement)
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfAccountExists, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Saldo insuficiente" })
+  };
+
+  const statementOperation = {
+    amount,
+    createdAt: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfAccountExists, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const formattedDate = new Date(date + "00:00");
+
+  const statement = customer.statement.filter((statement) => statement.createdAt.toDateString() === new Date(formattedDate).toDateString())
+
+  return response.json(statement)
 });
 
 app.listen(3333);
